0e8c967bae4630e8105948a106389ba396d2a7b8
mr-kup <kriskiddell+kupgh@gmail.com>
2024-12-25 12:59:10 +0000

    Roll with modifer now returns a RollResult.

--- cmd/dice.go
+++ cmd/dice.go
@@ -1,22 +1,97 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
+	"strconv"
+
+	"github.com/kriskiddell/plog"
 )
 
+type rollResult struct {
+	DiceRolls    []int
+	Modifier     int
+	ResultString string
+	Total        int
+}
+
 func main() {
-	fmt.Println(roll_dice(2000, 20))
-}
-
-func roll_die(sides int) int {
-	return rand.Intn(sides) + 1
-}
-
-func roll_dice(number int, sides int) []int {
-	results := make([]int, number)
-	for i := range number {
-		results[i] = roll_die(sides)
+	roll_result, err := rollWithModifier(2, 2, 1)
+	if err != nil {
+		plog.Error.Println(err)
 	}
-	return results
+
+	fmt.Print(roll_result.ResultString)
+
+}
+
+func rollDie(sides int) (int, error) {
+
+	if sides < 2 || sides > 1000 {
+		err := errors.New("Invalid number of sides.")
+		return 0, err
+	}
+
+	return rand.Intn(sides) + 1, nil
+
+}
+
+func rollDice(number int, sides int) ([]int, error) {
+
+	if number < 1 || number > 1000 {
+		return nil, errors.New("Invalid number of dice.")
+	}
+
+	if sides < 1 || sides > 1000 {
+		return nil, errors.New("Invalid number of sides.")
+	}
+
+	results := make([]int, number)
+
+	for i := range number {
+		result, err := rollDie(sides)
+		if err != nil {
+			return nil, err
+		}
+		results[i] = result
+	}
+
+	return results, nil
+}
+
+func rollWithModifier(number int, sides int, mod int) (rollResult, error) {
+	results, err := rollDice(number, sides)
+	if err != nil {
+		return rollResult{}, err
+	}
+
+	result := 0
+	result_string := fmt.Sprintf("%dd%d(", number, sides)
+	for i, r := range results {
+		result += r
+		result_string += strconv.Itoa(r)
+		if i < len(results)-1 {
+			result_string += " + "
+		} else {
+			result_string += ")"
+		}
+	}
+
+	if mod != 0 {
+		result += mod
+		result_string += fmt.Sprintf("%+d ", mod)
+	}
+
+	result_string += fmt.Sprintf("= %d", result)
+
+	rollResult := rollResult{
+		DiceRolls:    results,
+		Modifier:     mod,
+		Total:        result,
+		ResultString: result_string,
+	}
+
+	return rollResult, nil
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module dice
 
 go 1.22.5
+
+require github.com/kriskiddell/plog v0.0.0-20240611185332-d300bca3da92
